5f8580751ac4b4c7fad14a52ba43df2193757746
DⱥℝkͥAnͣgͫeℓ <60337323+Jijinr@users.noreply.github.com>
2021-01-10 19:35:19 +0530

    Call Back Added

---
+++ plugins/cb.py
@@ -0,0 +1,19 @@
+from pyrogram import CallbackQuery
+import pyrogram
+
+@pyrogram.Client.on_callback_query()
+async def button(bot, update):
+if "start" in cb_data:
+        await update.message.delete()
+        await start_user(bot, update.message)
+    elif "help_btn" in cb_data:
+        await update.message.delete()
+        await help_user(bot, update.message)
+    elif "cancel_btn" in cb_data:
+        await update.message.delete()
+    elif "upgrade_btn" in cb_data:
+        await update.message.delete()
+        await upgrade(bot, update.message)
+    elif "about_btn" in cb_data:
+        await update.message.delete()
+        await about(bot, update.message)
